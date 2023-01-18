5152f70915d846d6b931da6b655ba89d0c2dce7e
rushi6457 <rushiypatil@gmail.com>
2023-01-18 14:46:32 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Weather from './Components/Weather';
-import Search from './Components/Search';
 
 function App() {
   return (
     <div className="App">
         <Weather/>
-        <Search/>
     </div>
   );
 }
--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -1,44 +1,84 @@
-import { Box, Container, Input } from "@chakra-ui/react"
+import { Box, Container, Flex, Heading, Input, Text, position } from "@chakra-ui/react"
 import { getWeather } from "../redux/actions/weatherActions";
 import { useEffect, useState } from "react";
 import { WeatherData } from "../redux/Types/types";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
+import Forcast from "./Forcast";
+import Cards from "./Cards";
 
 interface WeatherProps{
     data:WeatherData
 }
-
-const Weather= () =>{
-    const WeatherData = useSelector(weather=>console.log(weather))
-    const [city,setCity] = useState<string | any>('')
-    const dispatch = useDispatch()
-    console.log(WeatherData);
-   
-const api_key = "f9e3ee87c2765b677ab21d53d1138a56";
-
-  const getData =async (city:string) =>{
-
-    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`)
-
-    let data = await res.json()
-
-    return data
+interface mapData{
+    name:string
+}
+interface Position {
+    coords: Coordinates;
 }
 
-  const handleChange = (e:any) =>{
+interface Coordinates {
+    latitude: number;
+    longitude: number;
+}
+
+declare var navigator: Navigator;
+
+interface Navigator {
+    geolocation: Geolocation;
+}
+
+interface Geolocation {
+    getCurrentPosition(success: (position: Position) => void, error?: (error: PositionErrorCallback) => void, options?: PositionOptions): void;
+}
+const Weather= ():JSX.Element =>{
+    const [city,setCity] = useState<string | any>('')
+    const [query,setQuery] = useState<string>('')
+    const dispatch = useDispatch()
+    const [data,setData] = useState<any>([])
+
+    function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+//https://api.openweathermap.org/data/2.5/weather?q=chalisgaon&appid=f9e3ee87c2765b677ab21d53d1138a56
+
+//https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=f9e3ee87c2765b677ab21d53d1138a56
+function showPosition(position:any) {
+       
+        
+    fetch(` https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=f9e3ee87c2765b677ab21d53d1138a56`)
+    .then((res)=>res.json())
+    .then((res)=>setCity(res.name))
+   
+}
+getLocation()
+
+
+useEffect(()=>{
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f9e3ee87c2765b677ab21d53d1138a56`)
+    .then((res)=>res.json())
+    .then((res)=>setData(res))
+},[])
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> | undefined = (e:any) =>{
       
     const {name,value} = e.target;
     setCity({...city,[name]:value})
-        // dispatch(getWeather(city))
+      
   }
  
   
+  
     return (
         <Box>
-            {/* <Container>
-                <Input name='city'  onChange={handleChange} placeholder="Search city" />
-            </Container> */}
+              <Heading>Weather Forecast</Heading>
+            <Forcast data={data} />
+            <Cards data = {data}/>
         </Box>
     )
 }
